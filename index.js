990b77b317e4fcf5f42393d3d548f9718970985a
anaconda24 <thatisnotthecase@gmail.com>
2017-03-06 13:19:12 +0300

    A

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-var sys = require('sys');
-var WebSocket = require('websocket').WebSocket;
-var ws = new WebSocket('ws://localhost:8000/biff', 'borf');
+/**
+ *  пользоваться монгой http://mongoosejs.com/docs/
+ */
 
-ws.addListener('data', function(buf) {
-    sys.debug('Got data: ' + sys.inspect(buf));
-});
-ws.onmessage = function(m) {
-    sys.debug('Got message: ' + m);
-}
 
-//https://www.npmjs.com/package/websocket-client
-//https://www.npmjs.com/package/mongoose
-//
